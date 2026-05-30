2173031b998631726aede0f89e98cee2e8439ab7
iryoma1120-commits <iryoma1120@gmail.com>
2026-05-31 01:08:01 +0900

    出力形式変更

--- src/utils/export.js
+++ src/utils/export.js
@@ -1,27 +1,20 @@
 export function generateTextOutput(date, part, piece, sessions, pults) {
   const isVn = (part === "Vn1st" || part === "Vn2nd");
-  const sess = sessions.find(s => s.date === date) || {};
 
-  let t = `【${part} 座席表】\n`;
-  t += `📅 ${date}`;
-  if (sess.time) t += `　${sess.time}`;
-  if (sess.place) t += `　${sess.place}`;
-  t += "\n";
-  if (isVn) t += `🎵 ${piece}\n`;
-  t += "─".repeat(16) + "\n";
-  t += `オモテ　　ウラ\n`;
+  let t = `【${part} ${piece}】\n`;
+  t += isVn ? `ウラ　　　オモテ\n` : `オモテ　　ウラ\n`;
 
   const lp = pults.filter(p => p.col === "L").sort((a, b) => a.row - b.row);
   for (let i = 0; i < lp.length; i++) {
     const pult = lp[i];
-    const omo = lastName(pult.slots[0] || "");
-    const ura = lastName(pult.slots[1] || "");
-    t += pad(omo, 6) + ura + "\n";
+    // Vnの場合は slots[0]がウラ、slots[1]がオモテとして格納されている
+    const left = lastName(pult.slots[0] || "－");
+    const right = lastName(pult.slots[1] || "－");
+    t += pad(left, 8) + right + "\n";
   }
 
-  t += "─".repeat(16) + "\n";
   const total = pults.reduce((s, p) => s + p.slots.filter(Boolean).length, 0);
-  t += `合計 ${total}名\n`;
+  t += `\n合計 ${total}名`;
 
   return t;
 }
